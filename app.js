3aa1864b2377b7ae13435da7c1240a955da0505a
kazim487 <kazimraza13579@gmail.com>
2026-05-02 21:53:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+let userInput = prompt("Enter your name")
+alert(userInput)
+
+let FatherName = prompt("Enter your father name")
+alert(FatherName)
+
+let brotherName = prompt("Enter your brother name")
+alert(brotherName)
+
+let yourAge = +prompt("Enter your age")
+alert(yourAge)
